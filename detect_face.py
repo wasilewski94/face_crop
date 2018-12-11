6fb68894779de13fbc5d432cfea6736ed4badbe0
wasilewski94 <wasilewskikrzysztof94@gmail.com>
2018-12-11 12:49:18 +0100

    Reading emotion labels and putting in the end of each image file name

--- detect_face.py
+++ detect_face.py
@@ -10,7 +10,7 @@ def listdir_nohidden(path):
 
 # detects faces in the image, crops it around the face in (w x h) size square and resizes it
 # returns 1 if face was detected, and 0 if not
-def crop_detected_faces(file_path, cropped_path, faceCascade, notdetected):
+def crop_detected_faces(file_path, cropped_file, faceCascade, notdetected):
     print(file_path)
     img = cv2.imread(str(file_path))
     gray = cv2.imread(str(file_path), 0)
@@ -27,7 +27,7 @@ def crop_detected_faces(file_path, cropped_path, faceCascade, notdetected):
         print(faces)
         cropped_img = cv2.resize(img[y: y + h, x: x + w], (width, heigth))
 
-    cv2.imwrite(str(cropped_path/file_path.name), cropped_img)
+    cv2.imwrite(str(cropped_file), cropped_img)
 
     if faces.any():
         return 1
@@ -50,18 +50,32 @@ heigth = 128
 #variables to calculate the efficiency
 notdetected = []
 detected = 0
+withoutemotion = 0
 
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 for f in sorted(listdir_nohidden(root_path)):
     for sf in sorted(listdir_nohidden(root_path/f)):
         files_names = [f for f in sorted(listdir_nohidden(root_path/f/sf))]
-        file_path = root_path/f/sf/files_names[-1]
-        d = crop_detected_faces(file_path, cropped_path, faceCascade, notdetected)
-        detected += d
+        if files_names[-1].endswith("emotion.txt"):
+            file_path = root_path/f/sf/files_names[-2] # -2 because emotions are there
+            file = open(str(root_path/f/sf/files_names[-1]), 'r')
+            emotion = int(file.readline().strip()[0]) #read emotion label from file
+            print("emotion label: " + str(emotion))
+            labeled_file_name = file_path.name[:-4]+'_'+str(emotion)+file_path.name[-4:]
+            cropped_labeled_path = Path(str(cropped_path) + '/' + labeled_file_name)
+            # if you use Windows comment next line
+            cropped_labeled_path = PurePosixPath(cropped_labeled_path)
+            print(cropped_labeled_path)
+
+            d = crop_detected_faces(file_path, cropped_labeled_path, faceCascade, notdetected)
+            detected += d
+        else:
+            withoutemotion += 1
 
 print("------------------------------------------------")
 print("Successfully cropped", detected, "images")
+print("Images with no emotion label:", withoutemotion)
 print(len(notdetected), "images with no face detected")
 print(*notdetected, sep='\n')
 print("However, remember to check if all of the faces were detected properly!")
